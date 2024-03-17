18a7e77e087b9426928c44988affaeb0d77fbcda
KZTahat <kztahat96@gmail.com>
2024-03-17 09:48:23 +0000

    feat: themes added

--- src/MyWedding.jsx
+++ src/MyWedding.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import { Howl } from "howler";
+
 import coupleDance from "./assets/GIFs/couple_dance_2.gif";
+
+// Background Images
+import pink from "./assets/images/pink.jpg";
+import green from "./assets/images/green.jpg";
+import brown from "./assets/images/brown.jpg";
+import purple from "./assets/images/purple.jpg";
+
+// Music
+import pinkMusic from "./assets/music/wedding_02.mp3";
+import sicilian_cafe from "./assets/music/sicilian-cafe.mp3";
 import classic from "./assets/music/classic.mp3";
 import romantic from "./assets/music/romantic.mp3";
-import sicilian_cafe from "./assets/music/sicilian-cafe.mp3";
-import wedding_1 from "./assets/music/wedding_01.mp3";
-import wedding_2 from "./assets/music/wedding_02.mp3";
+
 import "./MyWedding.css";
 
 function MyWedding() {
@@ -14,29 +23,24 @@ function MyWedding() {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
-  const [musicOn, setMusic] = useState(false);
+  let [theme, setTheme] = useState({
+    backgroundImage: pink,
+    music: new Howl({
+      src: [pinkMusic],
+    }),
+    color_one: "#01ACC4",
+    color_two: "#FA8C45",
+  });
 
-  let music = {
-    wedding_1: new Howl({
-      src: [wedding_1],
-    }),
-    classic: new Howl({
-      src: [classic],
-    }),
-    wedding_2: new Howl({
-      src: [wedding_2],
-    }),
-    sicilian_cafe: new Howl({
-      src: [sicilian_cafe],
-    }),
-    romantic: new Howl({
-      src: [romantic],
-    }),
-  };
-
-  const playMusic = () => {
-    music.wedding_2.play();
-    setMusic(true);
+  const switchTheme = (bcImage, newMusic, color_1, color_2) => {
+    setTheme({
+      backgroundImage: bcImage,
+      music: newMusic,
+      color_one: color_1,
+      color_two: color_2,
+    });
+    theme.music.stop();
+    newMusic.play();
   };
 
   function getCountDownTime() {
@@ -63,7 +67,14 @@ function MyWedding() {
   getCountDownTime();
 
   return (
-    <section className="count-down-timer-container">
+    <section
+      className="count-down-timer-container"
+      style={{
+        backgroundImage: `url("${theme.backgroundImage}")`,
+        "--color_one": theme.color_one,
+        "--color_two": theme.color_two,
+      }}
+    >
       <h1>Wedding Day</h1>
       <h2>
         Khaled <i className="ri-heart-fill"></i> Saba
@@ -93,13 +104,71 @@ function MyWedding() {
         <i className="ri-map-pin-2-line"></i> Palm Palace
       </h3>
       <img src={coupleDance} alt="couple dancing" />
-      <span
-        id={`music-on-${musicOn}`}
-        className="play-music"
-        onClick={playMusic}
-      >
-        <i className="ri-music-2-fill"></i>
-      </span>
+      <div className="theme-switch-buttons">
+        {/* Pink Theme */}
+        <span
+          className="theme-button"
+          style={{ backgroundImage: `url("${pink}")` }}
+          onClick={() =>
+            switchTheme(
+              pink,
+              new Howl({
+                src: [pinkMusic],
+              }),
+              "#01ACC4",
+              "#FA8C45"
+            )
+          }
+        />
+
+        {/* Green Theme */}
+        <span
+          className="theme-button"
+          style={{ backgroundImage: `url("${green}")` }}
+          onClick={() =>
+            switchTheme(
+              green,
+              new Howl({
+                src: [sicilian_cafe],
+              }),
+              "#23291E",
+              "#426C20"
+            )
+          }
+        />
+
+        {/* Brown Theme */}
+        <span
+          className="theme-button"
+          style={{ backgroundImage: `url("${brown}")` }}
+          onClick={() =>
+            switchTheme(
+              brown,
+              new Howl({
+                src: [classic],
+              }),
+              "#8C4D20",
+              "#350200"
+            )
+          }
+        />
+
+        {/* Purple Theme */}
+        <span
+          className="theme-button"
+          style={{ backgroundImage: `url("${purple}")` }}
+          onClick={() =>
+            switchTheme(
+              purple,
+              new Howl({
+                src: [romantic],
+              }),
+              "#CD6580",
+              "#423380"
+            )
+          }
+        />
+      </div>
     </section>
   );
 }
